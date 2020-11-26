0eaf1868190a0ade1c47a0e51cc185aef67b5d0f
mswati07 <74638741+mswati07@users.noreply.github.com>
2020-11-26 10:03:30 +0530

    Add files via upload

---
+++ camel-spring/src/main/java/com/swati/camelspring/MyRouteBuilder.java
@@ -0,0 +1,16 @@
+package com.swati.camelspring;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRouteBuilder extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		
+		from("file:inbox?noop=true")
+		.to("file:outbox");
+	}
+
+}
